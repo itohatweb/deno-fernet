8edb2ec77aa530770ab0761a874496c9466a7538
ITOH <to@itoh.at>
2021-09-03 12:57:33 +0200

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,8 @@
+export * as base64url from "https://deno.land/std@0.106.0/encoding/base64url.ts";
+export { Aes } from "https://deno.land/x/crypto@v0.10.0/aes.ts";
+export {
+  Cbc,
+  Padding,
+} from "https://deno.land/x/crypto@v0.10.0/block-modes.ts";
+export { hmac } from "https://deno.land/x/crypto@v0.10.0/hmac.ts";
+export type { SupportedAlgorithm } from "https://deno.land/x/crypto@v0.10.0/hmac.ts";
